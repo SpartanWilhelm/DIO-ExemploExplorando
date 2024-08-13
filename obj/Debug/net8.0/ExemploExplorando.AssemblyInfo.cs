40f52bd8927950d6114a637b53ba3c5cd501285f
César L. Guilherme <clguilherme3@gmail.com>
2024-08-13 09:58:00 -0300

    Refatoração

--- obj/Debug/net8.0/ExemploExplorando.AssemblyInfo.cs
+++ obj/Debug/net8.0/ExemploExplorando.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ExemploExplorando")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8508e491894aca6349746dfc5dcf5276710e3aa6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+15663b3dc47fa375ccff9fe27a701d014faa0ffa")]
 [assembly: System.Reflection.AssemblyProductAttribute("ExemploExplorando")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ExemploExplorando")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
