6993489a5754edbc7c53712e6de000d728d92a98
César L. Guilherme <clguilherme3@gmail.com>
2024-08-20 13:59:29 -0300

    Serializando um objeto

--- Program.cs
+++ Program.cs
@@ -1,12 +1,36 @@
 ﻿using ExemploExplorando.Models;
 using System.Globalization;
+using Newtonsoft.Json;
 
-int numero = 20;
-bool ehPar = false;
+//site para validação Json: https://codebeautify.org/jsonviewer
 
-//IF ternário
-ehPar = numero % 2 == 0;
-Console.WriteLine($"O número {numero} é " + (ehPar ? "par" : "ímpar"));
+//Serializando em Json
+
+//ISO 8601
+DateTime dataAtual = DateTime.Now;
+
+List<Venda> listaVendas = new List<Venda>();
+
+Venda v1 = new Venda(1, "Material de escritório", 25.00M, dataAtual);
+Venda v2 = new Venda(2, "Licença de software", 110.00M, dataAtual);
+
+listaVendas.Add(v1);
+listaVendas.Add(v2);
+
+string serializado = JsonConvert.SerializeObject(listaVendas, Formatting.Indented);
+
+File.WriteAllText("Arquivos/vendas.json",serializado);
+
+Console.WriteLine(serializado);
+
+//--------------
+
+// int numero = 20;
+// bool ehPar = false;
+
+// //IF ternário
+// ehPar = numero % 2 == 0;
+// Console.WriteLine($"O número {numero} é " + (ehPar ? "par" : "ímpar"));
 
 // //IF Não ternário
 // if (numero % 2 == 0)
--- obj/Debug/net8.0/ExemploExplorando.AssemblyInfo.cs
+++ obj/Debug/net8.0/ExemploExplorando.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ExemploExplorando")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cea793e06f3e43a7c5e8ac755080936f44534238")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+926c7982b6c60c90c51a02a367435e75c9132245")]
 [assembly: System.Reflection.AssemblyProductAttribute("ExemploExplorando")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ExemploExplorando")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
